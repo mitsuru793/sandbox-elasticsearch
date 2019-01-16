35b74946b5a0349f6fe43c134cf85392f8bf9314
mitsuru793 <mitsuru793@gmail.com>
2019-01-16 17:41:00 +0900

    add TermNotMatchTest

---
+++ tests/TermNotMatchTest.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+
+namespace EsExample;
+
+use Elastica\Client;
+use Elastica\Document;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @see https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-term-query.html
+ */
+final class TermNotMatchTest extends TestCase
+{
+    /** @var \Elastica\Index */
+    private $index;
+
+    /** @var \Elastica\Type */
+    private $type;
+
+    /** @var Client */
+    private $client;
+
+    /**
+     * @throws \Elastica\Exception\InvalidException
+     * @throws \Elastica\Exception\ResponseException
+     */
+    public function setUp()
+    {
+        $this->client = new Client();
+
+        $this->index = $this->client->getIndex('test-index');
+
+        if ($this->index->exists()) {
+            $this->index->delete();
+        }
+
+        $this->index->create([
+            'mappings' => [
+                '_doc' => [
+                    'properties' => [
+                        'full_text' => [
+                            'type' => 'text',
+                        ],
+                        'exact_value' => [
+                            'type' => 'keyword'
+                        ],
+                    ],
+                ],
+            ],
+        ], true);
+        $this->type = $this->index->getType('_doc');
+    }
+
+    public function testMain()
+    {
+        $doc = new Document(1, [
+            'full_text' => 'Quick Foxes!',
+            'exact_value' => 'Quick Foxes!'
+        ]);
+        $this->type->addDocument($doc);
+        $this->index->refresh();
+
+        $query['query']['term']['exact_value'] = 'Quick Foxes!';
+        $resultSet = $this->type->search($query);
+        $results = $resultSet->getDocuments();
+        $this->assertNotEmpty($results);
+        unset($query);
+
+        $query['query']['term']['full_text'] = 'Quick Foxes!';
+        $resultSet = $this->type->search($query);
+        $results = $resultSet->getDocuments();
+        $this->assertEmpty($results);
+        unset($query);
+
+        $query['query']['term']['full_text'] = 'foxes';
+        $resultSet = $this->type->search($query);
+        $results = $resultSet->getDocuments();
+        $this->assertNotEmpty($results);
+        unset($query);
+
+        $query['query']['match']['full_text'] = 'Quick Foxes!';
+        $resultSet = $this->type->search($query);
+        $results = $resultSet->getDocuments();
+        $this->assertNotEmpty($results);
+        unset($query);
+    }
+}
