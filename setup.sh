85df691fcb209a4585844579b5b290693eb2429f
mitsuru793 <mitsuru793@gmail.com>
2019-02-09 03:53:21 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ tar xvf elasticsearch-6.4.0.tar.gz
 
 wget https://artifacts.elastic.co/downloads/kibana/kibana-6.4.0-darwin-x86_64.tar.gz
 tar xvf kibana-6.4.0-darwin-x86_64.tar.gz
+
+curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.0.0-alpha1.tar.gz
+tar -xvf elasticsearch-7.0.0-alpha1.tar.gz
